3caa677fbcc2a3a6b33cdb3f016e347266487b2c
Apprentice <apprentice@dbc-chicago-023.local>
2014-02-06 10:09:15 -0600

    Add HTML file to load JS

--- zoo.js
+++ zoo.js
@@ -2,41 +2,55 @@
 // YOUR CODE: Create your Zoo "object literal" and Animal "constructor" and "prototypes" here.
 //------------------------------------------------------------------------------------------------------------------
 
-function Animal(name,length) {
-  this.name = name;
-  this.length = length;
+// Class with initialize constructor
+var Zoo = {} // empty fn?
 
-  // //break this out into a prototype
-  // this.identify = function() {
-  //   return "I am a " + this.name + " with " + this.length + " legs.";
-  // };
+// Truck in the animals
+Zoo.init = function(animals) {
+  this.animals = animals
+}
+
+// count number of two-legged animals
+Zoo.bipeds = function() {
+  var bipeds = []
+  for (var i = 0; i < this.animals.length; i++)
+  {
+    if(this.animals[i].legs == 2)
+    {
+      bipeds.push(animals[i])
+    }
+  }
+}
+
+// count number of four-legged animals
+Zoo.quadrupeds = function() {
+  var quadrupeds = []
+  for (var i = 0; i < this.animals.length; i++)
+  {
+    if(this.animals[i].legs == 2)
+    {
+      quadrupeds.push(animals[i])
+    }
+  }
+}
+
+// Animals needs to have name and # of legs
+function Animal(name,legs) {
+  this.name = name;
+  this.legs = legs;
 }; // end of fn animal
 
 // I am a prototype :)
 Animal.prototype.identify = function(){
-  return "I am a " + this.name + " with " + this.length + " legs.";
-};  // end of prototype
+  return "I am a " + this.name + " with " + this.legs + " legs.";
+};  // end of identify prototype
 
-// Class with initialize constructor
+// More prototype
+Animal.prototype.name = function(){
+  return this.name;
+};  // end of name prototype
 
-var Zoo = class({
-  // fill zoo!
-  init: function(truckload_o_animals)
-  {
-    this.animal = truckload_o_animals;
-  }, // must use comma here
-
-  bipeds: function()
-  {
-      // count number of two legged animals
-  },
-
-  quadrupeds: function()
-  {
-    // count number of four legged animals
-  },
-
-}); // end of Zoo
+// end of JS Zoo
 
 
 
@@ -44,7 +58,7 @@ var Zoo = class({
 // DRIVER CODE: Do **NOT** change anything below this point. Your task is to implement code above to make this work.
 //------------------------------------------------------------------------------------------------------------------
 
-function assert(test, message) {  
+function assert(test, message) {
   if (!test) {
     throw "ERROR: " + message;
   }
@@ -62,12 +76,12 @@ var animals = [
 
 Zoo.init(animals);
 
-// assert(
-//   Zoo.bipeds().length === 3, "the Zoo should have 3 bipeds"
-// );
-// assert(
-//   Zoo.quadrupeds().length === 2, "the Zoo should have 2 bipeds"
-// );
+assert(
+  Zoo.bipeds().legs === 3, "the Zoo should have 3 bipeds"
+);
+assert(
+  Zoo.quadrupeds().legs === 2, "the Zoo should have 2 bipeds"
+);
 assert(
   animals[0].identify() === "I am a Human with 2 legs.", "humans have 2 legs"
 );
